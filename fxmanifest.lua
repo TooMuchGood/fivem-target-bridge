75b648f07d2f6960adf6fd2a84c9b747c9d9e74d
TooMuchGood <desrochersvincent093@gmail.com>
2025-05-03 23:40:37 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+-- Libs obligatoires
+lua54 'yes'
+
+-- Dépendances
+dependency 'ox_lib'
+dependency 'ox_target'
+
+-- Scripts
+shared_script 'target_bridge.lua'
+client_script 'client/test_target.lua'
